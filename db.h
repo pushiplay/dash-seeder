6ce2ad8854e5e6344c6a6bee264daf2dd9c17904
pushiplay <puchicoin@yahoo.com>
2018-04-11 21:34:23 -0400

    Seeder for v1.1.6

--- db.h
+++ db.h
@@ -12,11 +12,11 @@
 
 #define MIN_RETRY 1000
 
-#define REQUIRE_VERSION 70209
+#define REQUIRE_VERSION 70210
 
 static inline int GetRequireHeight(const bool testnet = fTestNet)
 {
-    return testnet ? 0 : 15000;
+    return testnet ? 0 : 37512;
 }
 
 std::string static inline ToString(const CService &ip) {
